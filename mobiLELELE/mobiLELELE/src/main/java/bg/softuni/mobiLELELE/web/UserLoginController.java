99f4b18cd5695ca6ba36cf8f6b4842107c328811
Abu <iliyanananan@gmail.com>
2023-03-19 20:25:14 +0200

    Mobilelelele -----> add beginning of login implementation

--- mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserLoginController.java
+++ mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserLoginController.java
@@ -1,5 +1,7 @@
 package bg.softuni.mobiLELELE.web;
 
+import bg.softuni.mobiLELELE.model.dto.UserLoginDTO;
+import bg.softuni.mobiLELELE.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserLoginController {
 
+    private UserService userService;
+
+    public UserLoginController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users/login")
-    public String login(){
+    public String login() {
         return "auth-login";
     }
 
-   @PostMapping("/user/login")
-    public String login(){
-
-   }
+    @PostMapping("/user/login")
+    public String login(UserLoginDTO userLoginDTO) {
+        System.out.println("User is logged: " + userService.login(userLoginDTO));
+        return "redirect:/";
+    }
 }
