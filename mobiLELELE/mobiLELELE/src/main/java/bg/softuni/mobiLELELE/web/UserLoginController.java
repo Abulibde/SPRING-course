533e9915449480c986058209630c45b58791c0ee
Abu <iliyanananan@gmail.com>
2023-03-19 19:46:16 +0200

    Mobilelelele -----> add softUni resources

---
+++ mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserLoginController.java
@@ -0,0 +1,4 @@
+package bg.softuni.mobiLELELE.web;
+
+public class UserLoginController {
+}
---
+++ mobiLELELE/mobiLELELE/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+USE mobilelele;
+
+INSERT INTO users(id, email, first_name, image_url, is_active, last_name, password)
+VALUES (1,'luchezar.balev@gmail.com','Lucho',null,1,'Balev','topsecret');
