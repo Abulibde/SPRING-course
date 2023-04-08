671a545e2ed28ec7f51c57b8e7ba0d8295e5f233
Abu <iliyanananan@gmail.com>
2023-04-08 14:21:33 +0300

    Mobilelelele -----> add beginning of registrationController

--- mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserLoginController.java
+++ mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserLoginController.java
@@ -5,8 +5,10 @@ import bg.softuni.mobiLELELE.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserLoginController {
 
     private UserService userService;
@@ -15,18 +17,18 @@ public class UserLoginController {
         this.userService = userService;
     }
 
-    @GetMapping("/users/login")
+    @GetMapping("/login")
     public String login() {
         return "auth-login";
     }
 
-    @GetMapping("/user/logout")
+    @GetMapping("/logout")
     public String logout() {
         userService.logout();
         return "redirect:/";
     }
 
-    @PostMapping("/user/login")
+    @PostMapping("/login")
     public String login(UserLoginDTO userLoginDTO) {
         userService.login(userLoginDTO);
         //System.out.println("User is logged: " + userService.login(userLoginDTO));
---
+++ mobiLELELE/mobiLELELE/src/main/java/bg/softuni/mobiLELELE/web/UserRegistrationController.java
@@ -0,0 +1,8 @@
+package bg.softuni.mobiLELELE.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserRegistrationController {
+
+}
